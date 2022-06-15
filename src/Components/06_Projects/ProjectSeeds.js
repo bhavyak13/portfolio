794f905b4f0effa82adce2ba866739f9dd48c494
Bhavya Kawatra <bhavyakawatra6@gmail.com>
2022-06-15 15:09:09 +0530

    Portfolio Component added

---
+++ src/Components/06_Projects/ProjectSeeds.js
@@ -0,0 +1,19 @@
+const projects = [
+    {
+        githubLink: 'https://github.com/bhavyak13/yelpCamp',
+        img: 'https://raw.githubusercontent.com/bhavyak13/yelpCamp/master/images/homePage.png'
+    },
+    {
+        githubLink: 'https://github.com/bhavyak13/React-Colors-Project',
+        img: 'https://i.imgur.com/GM0etHA.png'
+    },
+    {
+        githubLink: 'https://github.com/bhavyak13/Work-board',
+        img: 'https://raw.githubusercontent.com/bhavyak13/Work-board/master/images/index.png'
+    },
+    {
+        githubLink: 'https://github.com/bhavyak13/Shops-App',
+        img: 'https://raw.githubusercontent.com/bhavyak13/Shops-App/master/images/ShopList.png',
+    },
+];
+export {projects};
--- src/Components/06_Projects/Projects.js
+++ src/Components/06_Projects/Projects.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { projects } from './ProjectSeeds.js'
+import './Projects.css'
 
 const Projects = () => {
   return (
     <section id='project'>
-      
+      <h5>My recent Work</h5>
+      <h2>My Projects</h2>
+      <div className='container project__container'>
+        {projects.map((p, i) => (
+          <article className='project__item' key={i}>
+            <div className='project__item-title'>
+              <h3>{p.githubLink.slice(29)}</h3>
+              <a href={p.githubLink} className='btn' target='_blank' rel='noreferrer'>Github</a>
+            </div>
+            <div className='project__item-img'>
+              <img src={p.img} alt={p.img} />
+            </div>
+          </article>
+        ))}
+      </div>
     </section>
   )
 }
